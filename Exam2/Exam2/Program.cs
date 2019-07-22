9649a1a2f1719e23b3e8fe826ebc13807febefba
bachlqth1807027 <bachlqth1807027@fpt.edu.vn>
2019-07-22 15:25:59 +0700

    bai hti c#

---
+++ Exam2/Exam2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string st; //khai bao chuoi ban dau
+            string word; //khai bao chuoi con can tim
+            int strt = 0;
+            int cnt = -1;
+            int idx = -1;
+            
+            Console.WriteLine("Enter a String");
+            st = Console.ReadLine();
+            Console.WriteLine("Enter a word to search");
+            word = Console.ReadLine();
+            
+            while (strt != -1)
+            {
+                strt = st.IndexOf(word, idx + 1);
+                cnt += 1;
+                idx = strt;
+            }
+            Console.Write("Word  '{0}' found " + cnt + " time in the string.\n", word);  
+
+            Console.ReadKey();
+        }
+    }
+}
